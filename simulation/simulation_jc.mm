2c83a65d3ed161cecf40ff0cae8e3fbf9c276cda
arabian9ts <nok14.tm59@gmail.com>
2017-06-07 23:57:11 +0900

    comment block added

--- simulation/simulation_jc.mm
+++ simulation/simulation_jc.mm
@@ -1,42 +1,53 @@
+/*---------- solver func ----------*/
 Func Matrix vdpol(t,x,u)
+/* argments declaration */
 Real t;
 Matrix x,u;
 {
-Real m,l,J,c,a,c1,c2,g;
-Real th1,th2;
-Matrix dth;
+  /* local declaration */
+    Real m,l,J,c,a,c1,c2,g;
+    Real th1,th2;
+    Matrix dth;
 
-m = 0.038;
-l = 0.12;
-J = 3.7e-4;
-c = 7.34e-5;
-g = 9.8;
+    /* variations to be changed */
+    J = 3.9e-4;
+    c = 9.8e-5;
 
-th1 = x(1,1);
-th2 = x(2,1);
+    /* constant variations */
+    m = 0.038;
+    l = 0.12;
+    g = 9.8;
+    u = [0.0];
 
-u = [0.0];
+    /* trans to matrix operation */
+    th1 = x(1,1);
+    th2 = x(2,1);
 
-dth = [[- c * th1 / (J + m * l * l) - m * g * l * th2 / (J + m * l *l ) ]
-      [                          th1                                  ]];
+    /* operating differential */
+    dth = [[- c * th1 / (J + m * l * l) - m * g * l * th2 / (J + m * l *l ) ]
+          [                          th1                                  ]];
 
-return dth;
+    return dth;
 }
+/*-------------- end --------------*/
 
-
+/*---------- --main func ----------*/
 Func void main()
 {
-Matrix X,T,x0;
-Real t0,t1,h,dth0;
-Matrix vdpol();
-Matrix data;
-read data << "drop.mat";
-dth0 = -0.01;
-x0=[PI-2.90754400E+00 ,dth0]';
-t0 = 0.0;
-t1 = 10.0;
-h =  0.01;
-{T,X} = Ode(t0,t1,x0,vdpol,"",h);
-mgplot(1,T,X(1,:),{"th"});
-mgreplot(1,data(1,:),data(4,:) .+ PI);
+    /* local declaration */
+    Matrix X,T,x0;
+    Real t0,t1,h,dth0;
+    Matrix vdpol();
+    Matrix data;
+
+    /* plot process */
+    read data << "maemaemae.mat";
+    dth0 = 0.00;
+    x0=[PI+data(4,1) ,dth0]';
+    t0 = 0.0;
+    t1 = 5.0;
+    h =  0.01;
+    {T,X} = Ode(t0,t1,x0,vdpol,"",h);
+    mgplot(1,T,X(1,:),{"th"});
+    mgreplot(1,data(1,:),data(4,:) .+ PI);
 }
--- simulation/simulation_mf.mm
+++ simulation/simulation_mf.mm
@@ -1,40 +1,51 @@
+/*---------- solver func ----------*/
 Func Matrix vdpol(t,x,u)
+/* argments declaration */
 Real t;
 Matrix x,u;
 {
-Real M,f,l,a,c1,c2,g;
-Real r1,r2;
-Matrix dr;
+    /* local declaration */
+    Real M,f,l,a,c1,c2,g;
+    Real r1,r2;
+    Matrix dr;
 
-M = 1.02;
-f = 9.78;
-a = 0.49;
-g = 9.8;
+    /* variations to be changed */
+    M = 1.001;
+    f = 9.67;
+    u = [13.0];
 
-r1 = x(1,1);
-r2 = x(2,1);
+    /* constant variations */
+    a = 0.49;
+    g = 9.8;
 
-u = [11.0];
+    /* trans to matrix operation */
+    r1 = x(1,1);
+    r2 = x(2,1);
 
-dr = [[a*u(1)/M - f*r1/M ]
-      [        r1        ]];
+    /* operating differential */
+    dr = [[a*u(1)/M - f*r1/M ]
+          [        r1        ]];
 
-return dr;
+    return dr;
 }
+/*-------------- end --------------*/
 
-
+/*---------- --main func ----------*/
 Func void main()
 {
-Matrix X,T,x0;
-Real t0,t1,h;
-Matrix vdpol();
-Matrix data;1
-read data << "step_11.mat";
-x0=[0 0]';
-t0 = 0.0;
-t1 = ;
-h =  0.01;
-{T,X} = Ode(t0,t1,x0,vdpol,"",h);
-mgplot(1,T,X(2,:),{"r"});
-mgreplot(1,data(1,:),data(3,:));
+    /* local declaration */
+    Matrix X,T,x0;
+    Real t0,t1,h;
+    Matrix vdpol();
+    Matrix data;
+
+    /* plot process */
+    read data << "restep13.mat";
+    x0=[0 0]';
+    t0 = 0.0;
+    t1 = 0.5;
+    h =  0.01;
+    {T,X} = Ode(t0,t1,x0,vdpol,"",h);
+    mgplot(1,T,X(2,:),{"dr"});
+    mgreplot(1,data(1,:),data(3,:));
 }
