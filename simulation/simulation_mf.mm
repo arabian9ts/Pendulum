8a0af80c1c3a6f0cc0f69b0e30930f5b6bf6df81
arabian9ts <nok14.tm59@gmail.com>
2017-06-08 15:36:38 +0900

    simulation files refactored

--- simulation/simulation_mf.mm
+++ simulation/simulation_mf.mm
@@ -1,10 +1,10 @@
 /*---------- solver func ----------*/
 Func Matrix vdpol(t,x,u)
-/* argments declaration */
+/* argments declarations */
 Real t;
 Matrix x,u;
 {
-    /* local declaration */
+    /* local declarations */
     Real M,f,l,a,c1,c2,g;
     Real r1,r2;
     Matrix dr;
@@ -19,12 +19,12 @@ Matrix x,u;
     g = 9.8;
 
     /* trans to matrix operation */
-    r1 = x(1,1);
-    r2 = x(2,1);
+    r1 = x(1);
+    r2 = x(2);
 
     /* operating differential */
-    dr = [[a*u(1)/M - f*r1/M ]
-          [        r1        ]];
+    dr = [[        r2         ]
+          [ a*u(1)/M - f*r2/M ]];
 
     return dr;
 }
@@ -36,7 +36,6 @@ Func void main()
     /* local declaration */
     Matrix X,T,x0;
     Real t0,t1,h;
-    Matrix vdpol();
     Matrix data;
 
     /* plot process */
@@ -46,6 +45,6 @@ Func void main()
     t1 = 0.5;
     h =  0.01;
     {T,X} = Ode(t0,t1,x0,vdpol,"",h);
-    mgplot(1,T,X(2,:),{"dr"});
+    mgplot(1,T,X(1,:),{"dr"});
     mgreplot(1,data(1,:),data(3,:));
 }
