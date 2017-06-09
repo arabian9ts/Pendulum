c98e909b22445a5e641e9e143775576c72a3bd8d
arabian9ts <nok14.tm59@gmail.com>
2017-06-09 11:32:55 +0900

    original model file added

---
+++ simulation/matx/diff_eqs.mm
@@ -0,0 +1,115 @@
+
+Func Matrix diff_eqs_nonliner(t, xx, u)
+Real t;
+Matrix xx, u;
+{
+  Matrix K;
+  Matrix xxdot, z;
+  Matrix tmp, tmp2, tmp3;
+  Real a, m, M, g, l, f, c, J;
+
+  a = 0.73;
+  m = 0.038;
+  M = 1.49;
+  g = 9.8;
+  l = 0.13;
+  f = 15.10;
+  c = 2.1e-4;
+  J = 4.5e-4;
+
+  u = [0];
+
+/*
+  K=[[ M+m,           m*l*cos(xx(2)) ]
+     [ m*l*cos(xx(2)),    J+m*l*l    ]];
+
+  xxdot = [ [ xx(3) ]
+            [ xx(4) ]
+            [
+             (K~ * [[ a*u(1) + m*l*sin(xx(2)) * xx(4)^2 - f*xx(3) ]
+                    [           m*g*l*sin(xx(2)) - c*xx(4)          ]])
+            ]
+          ];
+
+  K=[[ M+m,           m*l*cos(xx(2)) ]
+     [ m*l*cos(xx(2)),    J+m*l*l    ]];
+
+*/
+
+  xxdot = [ [ xx(3) ]
+            [ xx(4) ]
+            [
+             (K~ * [[ a*u(1) + m*l*sin(xx(2)) * xx(4)^2 - f*xx(3) ]
+                    [           m*g*l*sin(xx(2)) - c*xx(4)          ]])
+            ]
+          ];
+
+  return xxdot;
+}
+
+Func Matrix diff_eqs_liner_under(t, xx, u)
+Real t;
+Matrix xx, u;
+{
+  Matrix K, _K, A, B;
+  Matrix tmp, tmp2, tmp3;
+  Matrix xxdot;
+  Real a, m, M, g, l, f, c, J;
+
+  a = 0.49;
+  m = 0.038;
+  M = 1.02;
+  g = 9.8;
+  l = 0.12;
+  f = 9.78;
+  c = 7.34e-5;
+  J = 3.7e-4;
+
+  u = [0];
+
+  K=[[ M+m,      -m*l   ]
+     [ -m*l,    J+m*l*l ]];
+
+  /** inv(K) */
+  _K=K~;
+
+  tmp=_K*[[ 0,    0    ]
+          [ 0,  -m*g*l ]];
+
+  tmp2=_K*[[ -f,   0  ]
+           [  0,   -c ]];
+
+  A=[[ Z(2,2) I(2,2) ]
+     [ tmp    tmp2   ]];
+  // print A;
+
+  tmp3=_K*[[a] [0]];
+  B=trans([0 0 tmp3(1) tmp3(2)]);
+  // print B;
+
+  xxdot=A*xx+B*u;
+
+  return xxdot;
+}
+
+Func void main()
+{
+  Real t0,t1,h;
+  Matrix x0,T,X;
+  Matrix data;
+
+  x0=trans([0 10.0*PI/(180) 0 0]);
+  t0=0.0;
+  t1=10.0;
+  h=0.01;
+
+  // print diff_eqs_nonliner(1,x0,[0]);
+
+  {T,X}=Ode(t0,t1,x0,diff_eqs_liner_under,"",h);
+
+  read data << "drop.mat";
+
+  mgplot(1,T,X,{"r","th","dr","dth"});
+  mgreplot(1,data(1,:),data(4,:).-(data(4,1)-X(3,1)));
+  mgplot_eps(1,"nonliner.eps");
+}
