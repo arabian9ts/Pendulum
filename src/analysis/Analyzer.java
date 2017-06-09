40ddff58b9a28d19060b3b44faea872ccf15b59b
maeda <maeda@mk.ces.kyutech.ac.jp>
2017-06-09 14:43:35 +0900

    analyzer added

---
+++ src/analysis/Analyzer.java
@@ -0,0 +1,73 @@
+package analysis;
+
+import org.mklab.tool.control.Ctrm;
+import org.mklab.tool.control.LinearSystem;
+import org.mklab.tool.control.Obsm;
+import org.mklab.nfc.matrix.*;
+import model.LinearPendulum;
+
+import model.LinearPendulum;
+
+/**
+ * 線形システムの解析を行うクラスです
+ * @author maeda
+ *
+ */
+public class Analyzer {
+	
+	/**
+	 * 線形システム
+	 */
+	private LinearSystem linearSystem;
+	
+	/**
+	 * メインメソッド
+	 * @param args コマンドライン引数
+	 */
+	public static void main(String[] args) {
+		Analyzer analizer = new Analyzer(new LinearPendulum().getLinearSystem());
+	}
+	
+	/**
+	 * コンストラクタ
+	 * @param linearSystem 解析対象の線形システム
+	 */
+	public Analyzer(LinearSystem linearSystem){
+		this.linearSystem = linearSystem;
+	}
+	
+	/**
+	 * システムの極と安定性判別結果を表示します
+	 */
+	public void showStability(){
+		final DoubleMatrix A = (DoubleMatrix)this.linearSystem.getA();
+		final Matrix poles = A.eigenValue();
+		
+		// 安定性判別結果の表示処理
+	}
+	
+	/**
+	 * システムの可制御性行列のランクと可制御性判別結果を表示します
+	 */
+	public void showControllability(){
+		final Matrix A = this.linearSystem.getA();
+		final Matrix B = this.linearSystem.getB();
+		final DoubleMatrix Nc = (DoubleMatrix)Ctrm.ctrm(A, B);
+		
+		// システムの可制御性行列のランクと可制御性判別結果を表示します
+		
+	}
+	
+	/**
+	 * システムの可観測性行列のランクと可観測性判別結果を表示します
+	 */
+	public void showObservability(){
+		final Matrix A = this.linearSystem.getA();
+		final Matrix B = this.linearSystem.getB();
+		final DoubleMatrix No = (DoubleMatrix)Obsm.obsm(A, B);
+		
+		// システムの可観測性行列のランクと可観測性判別結果を表示します
+		
+	}
+	
+}
