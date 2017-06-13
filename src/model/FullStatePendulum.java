ca897d2367ec985b671cf9eaa4bc8b04060fb3b7
maeda <maeda@mk.ces.kyutech.ac.jp>
2017-06-13 13:08:21 +0900

    FullStatePendulum implemented

--- src/model/FullStatePendulum.java
+++ src/model/FullStatePendulum.java
@@ -1,5 +1,26 @@
 package model;
 
-public class FullStatePendulum {
+import org.mklab.nfc.matrix.Matrix;
 
+/**
+ * 倒立振子の非線形モデル(全状態観測可能)を表すクラスです
+ * @author maeda
+ *
+ */
+public class FullStatePendulum extends Pendulum {
+	
+	/**
+	 * コンストラクター
+	 */
+	public FullStatePendulum(){
+		super();
+		setOutputSize(4);
+	}
+	
+	/**
+	 * @see model.Pendulum#outputEquation(double, org.mklab.nfc.matrix.Matrix)
+	 */
+	public Matrix outputEquation(final double t, final Matrix x){
+		return x;
+	}
 }
