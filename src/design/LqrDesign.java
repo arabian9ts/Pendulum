e0e65748c4c9bb6ee031346f67d6287fe3d213ab
maeda <maeda@mk.ces.kyutech.ac.jp>
2017-06-13 12:03:15 +0900

    LqrDesign implemented

--- src/design/LqrDesign.java
+++ src/design/LqrDesign.java
@@ -1,5 +1,29 @@
 package design;
 
+import org.mklab.nfc.matrix.DoubleMatrix;
+import org.mklab.nfc.matrix.Matrix;
+import org.mklab.tool.control.system.controller.LqrDesigner;
+
+import model.LinearPendulum;
+
+/**
+ * LQ最適制御(安定化)のための状態フィードバックを設計するクラスです
+ * @author maeda
+ *
+ */
 public class LqrDesign {
 
+	/**
+	 * メインメソッド
+	 * @param args コマンドライン引数
+	 */
+	public static void main(String[] args) {
+		LqrDesigner designer = new LqrDesigner(new LinearPendulum());
+		Matrix Q = DoubleMatrix.diagonal(new double[]{1.0E0, 1.0E0, 1, 1});
+		Matrix R = new DoubleMatrix(new double[]{1});
+		designer.setWeightingMatrices(Q, R);
+		designer.getStateFeedback().print("F: LQ最適制御のための状態フィードバック行列"); //$NON-NLS-1$
+		System.out.println(""); //$NON-NLS-1$
+		designer.showClosedLoopPoles();
+	}
 }
