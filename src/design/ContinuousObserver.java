852e56c93b03ef11fd7978f6e1007c00e323d457
arabian9ts <nok14.tm59@gmail.com>
2017-06-16 00:10:13 +0900

    implement: ContinuousObserver

--- src/design/ContinuousObserver.java
+++ src/design/ContinuousObserver.java
@@ -1,5 +1,23 @@
 package design;
 
-public class ContinuousObserver {
+import org.mklab.nfc.matrix.DoubleComplexMatrix;
 
+import model.LinearPendulum;
+
+/**
+ * (1重)倒立振子の連続時間オブザーバを表すクラスです
+ * @author arabian9ts
+ *
+ */
+public class ContinuousObserver extends org.mklab.tool.control.system.controller.ContinuousObserver {
+	
+	/**
+	 * オブジェクトを初期化します
+	 */
+	public ContinuousObserver(){
+		super(new LinearPendulum());
+		setObserverPoles(new DoubleComplexMatrix(
+				new double[]{-2, -2},
+				new double[]{0, 0}).transpose());
+	}
 }
