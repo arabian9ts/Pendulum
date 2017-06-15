308a9f1e6b91bcff7734f8670cb74d98dead21ea
Fumiya Takikawa <takikawafumiya@gmail.com>
2017-06-15 12:46:07 +0900

    LQ最適制御のための状態フィードバックを設計するdesing_lqr を追加しました。

---
+++ simulation/matx/design_lqr.mm
@@ -0,0 +1,76 @@
+read <- "params";
+Matrix A, B, F;
+
+
+Func void init()
+{
+  Matrix K, A21, A22, B2, Q, R, P;
+
+  K = [[M+m ,m*l]
+       [m*l, J+m*l^2]];
+
+  A21=K\[[0 0][0, m*g*l]];
+  A22=K\[[-f, 0][0, -c]];
+  B2=K\[[a][0]];
+  A  = [[Z(2,2), I(2,2)]
+       [ A21 ,  A22 ]];
+  B  = trans([Z(1,2),trans(B2)]);
+
+  Q = diag(1, 1, 1, 1);
+  R = [1];
+  read Q;
+  {F, P} = lqr(A ,B ,Q, R);
+}
+
+Func Matrix link_eqs_lqr(t, x)
+Matrix x;
+Real t;
+{
+  Matrix u, xref;
+
+  // 台車の可動範囲に関する制限
+	if (x(1,1) <= -0.16 || 0.16 <= x(1,1)) { // r = x(1,1)
+        OdeStop();	                         // シミュレーションを停止
+  }
+
+  xref = [0 0 0 0]';
+  u = F * (xref - x);
+
+  // 入力の大きさに関する制限
+  if (u(1,1) <= -15) {
+      u(1,1) = -15;
+  } else if (u(1,1) >= 15) {
+      u(1,1) = 15;
+  }
+
+  return u;
+}
+
+
+Func Matrix diff_eqs_linear(t,x,u)
+Real t;
+Matrix x,u;
+{
+  return A*x + B*u;
+}
+
+Func void main()
+{
+
+  Real t0, t1, r0 ,th0, tol;
+  Matrix x0, T, X, U;
+
+  t0 = 0.0;
+  t1 = 1.0;
+  r0 = 0.0;
+  th0 = 10.0;
+  x0 = [r0 th0/180*PI 0 0]';
+  tol = 1.0E-8;
+
+  init();
+
+  {T, X, U} = Ode45Auto(t0, t1, x0, diff_eqs_linear, link_eqs_lqr, tol);
+
+  mgplot(1, T, X, {"r","th","dr","dth"});
+  mgreplot(1, T, U, {"u"});
+}
