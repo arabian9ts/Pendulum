ec87920b6e5f107f33a2b47173e0f729515a88a6
arabian9ts <nok14.tm59@gmail.com>
2017-06-20 14:18:37 +0900

    implement: src/model/PolePlaceStateFeedbackPendulum

--- src/model/PolePlaceStateFeedbackPendulum.java
+++ src/model/PolePlaceStateFeedbackPendulum.java
@@ -1,5 +1,29 @@
 package model;
 
-public class PolePlaceStateFeedbackPendulum {
+import org.mklab.nfc.matrix.DoubleComplexMatrix;
+import org.mklab.nfc.matrix.Matrix;
+import org.mklab.tool.control.system.SystemOperator;
 
+/**
+ * 倒立振子の(極配置に基づく状態フィードバック+離散時間オブザーバ)による安定化制御のシステムを生成するクラスです
+ * 
+ * @author arabian9ts
+ *
+ */
+public class PolePlaceStateFeedbackPendulum {
+	
+	/**
+	 * 倒立振子の(極配置に基づく状態フィードバック+離散時間オブザーバ)による安定化制御のシステムを返します
+	 * @return 倒立振子の(極配置に基づく状態フィードバック+離散時間オブザーバ)による安定化制御のシステム
+	 */
+	public static SystemOperator getInstance(){
+		Matrix closedLoopPoles = new DoubleComplexMatrix(
+				new double[]{-1, -1, -2, -2},
+				new double[]{0, 0, 0, 0}).transpose();
+		
+		PolePlaceStateFeedback stateFeedback = new PolePlaceStateFeedback();
+		stateFeedback.setClosedLoopPoles(closedLoopPoles);
+		
+		return StateFeedbackPendulum.getInstance(stateFeedback);
+	}
 }
