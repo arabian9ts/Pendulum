3925829ddc897e3108867b68925ad060e159f294
arabian9ts <nok14.tm59@gmail.com>
2017-06-20 14:32:23 +0900

    modify: src/model/LqrStateFeedbackPendulum, src/simulation/LqrStateFeedbackSimulation

--- src/model/LqrStateFeedbackPendulum.java
+++ src/model/LqrStateFeedbackPendulum.java
@@ -1,5 +1,27 @@
 package model;
 
-public class LqrStateFeedbackPendulum {
+import org.mklab.nfc.matrix.DoubleMatrix;
+import org.mklab.nfc.matrix.misc.DiagonalMatrix;
+import org.mklab.tool.control.system.SystemOperator;
 
+/**
+ * 倒立振子のLQR最適制御に基づく状態フィードバックによる安定化制御のシステムを生成するクラスです
+ * 
+ * @author arabian9ts
+ *
+ */
+public class LqrStateFeedbackPendulum {
+	
+	/**
+	 * 倒立振子のLQR最適制御に基づく状態フィードバックによる安定化制御のシステムを返します
+	 * @return 倒立振子のLQR最適制御に基づく状態フィードバックによる安定化制御のシステム
+	 */
+	public static SystemOperator getInstance(){
+		DoubleMatrix Q = DiagonalMatrix.create(new double[]{1.0E0, 1.0E0, 1, 1});
+		DoubleMatrix R = new DoubleMatrix(new double[]{1});
+		LqrStateFeedback stateFeedback = new LqrStateFeedback();
+		stateFeedback.setWeightingMatrices(Q, R);
+		
+		return StateFeedbackPendulum.getInstance(stateFeedback);
+	}
 }
--- src/simulation/LqrStateFeedbackSimulation.java
+++ src/simulation/LqrStateFeedbackSimulation.java
@@ -1,26 +1,49 @@
 package simulation;
 
-import org.mklab.nfc.matrix.DoubleMatrix;
-import org.mklab.nfc.matrix.misc.DiagonalMatrix;
-import org.mklab.tool.control.system.SystemOperator;
+import java.io.IOException;
 
-import model.LqrStateFeedback;
-import model.StateFeedbackPendulum;
+import org.mklab.nfc.matrix.DoubleMatrix;
+import org.mklab.nfc.ode.RungeKuttaFehlberg;
+import org.mklab.nfc.util.Pause;
+import org.mklab.tool.control.system.SystemOperator;
+import org.mklab.tool.control.system.SystemSolver;
+import org.mklab.tool.graph.gnuplot.Canvas;
+import org.mklab.tool.graph.gnuplot.Gnuplot;
+
+import model.LqrStateFeedbackPendulum;
 
 /**
- * 倒立振子のLQR最適制御に基づく状態フィードバックによる安定化制御のシステムを生成するクラスです
+ * 倒立振子のLQR最適制御に基づく状態フィードバックによる安定化制御のシミュレーションを実行するクラスです
  * 
  * @author arabian9ts
  *
  */
 public class LqrStateFeedbackSimulation {
 	
-	public static SystemOperator getInstance(){
-		DoubleMatrix Q = DiagonalMatrix.create(new double[]{1.0E0, 1.0E0, 1, 1});
-		DoubleMatrix R = new DoubleMatrix(new double[]{1});
-		LqrStateFeedback stateFeedback = new LqrStateFeedback();
-		stateFeedback.setWeightingMatrices(Q, R);
+	/**
+	 * @param args コマンドライン引数
+	 * @throws InterruptedException 強制終了された場合
+	 * @throws IOException キーボードから入力できない場合
+	 */
+	public static void main(String[] args) throws InterruptedException, IOException {
 		
-		return StateFeedbackPendulum.getInstance(stateFeedback);
+		SystemOperator system = LqrStateFeedbackPendulum.getInstance();
+		RungeKuttaFehlberg solver = new RungeKuttaFehlberg();
+		solver.setAbsoluteTolerance(1.0E-8);
+		new SystemSolver(solver).solveAuto(system, 0.0, 10.0);
+		DoubleMatrix t = solver.getTimeSeries();
+		DoubleMatrix y = solver.getOutputSeries();
+		
+		Gnuplot gnuplot = new Gnuplot();
+		Canvas canvas = gnuplot.createCanvas();
+		canvas.setGridVisible(true);
+		canvas.plot(t, (DoubleMatrix)y.getRowVectors(1, 2), new String[]{"r", "th"});  //$NON-NLS-1$//$NON-NLS-2$
+		Pause.pause();
+		gnuplot.close();
+		
+		System.exit(0);
+		
 	}
+
+
 }
