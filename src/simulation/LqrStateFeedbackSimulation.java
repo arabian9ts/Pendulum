8aeaa5f85f166f8c93512af5867a9a15ea4a259d
arabian9ts <nok14.tm59@gmail.com>
2017-06-20 14:23:35 +0900

    implement: src/simulation/LqrStateFeedbackSimulation

--- src/simulation/LqrStateFeedbackSimulation.java
+++ src/simulation/LqrStateFeedbackSimulation.java
@@ -1,5 +1,26 @@
 package simulation;
 
-public class LqrStateFeedbackSimulation {
+import org.mklab.nfc.matrix.DoubleMatrix;
+import org.mklab.nfc.matrix.misc.DiagonalMatrix;
+import org.mklab.tool.control.system.SystemOperator;
 
+import model.LqrStateFeedback;
+import model.StateFeedbackPendulum;
+
+/**
+ * 倒立振子のLQR最適制御に基づく状態フィードバックによる安定化制御のシステムを生成するクラスです
+ * 
+ * @author arabian9ts
+ *
+ */
+public class LqrStateFeedbackSimulation {
+	
+	public static SystemOperator getInstance(){
+		DoubleMatrix Q = DiagonalMatrix.create(new double[]{1.0E0, 1.0E0, 1, 1});
+		DoubleMatrix R = new DoubleMatrix(new double[]{1});
+		LqrStateFeedback stateFeedback = new LqrStateFeedback();
+		stateFeedback.setWeightingMatrices(Q, R);
+		
+		return StateFeedbackPendulum.getInstance(stateFeedback);
+	}
 }
