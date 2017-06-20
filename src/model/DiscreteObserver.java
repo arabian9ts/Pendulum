dbea9ca18670dcd33a28fe4b54a5c8f44854ae71
arabian9ts <nok14.tm59@gmail.com>
2017-06-20 11:39:37 +0900

    implement: src/model/DiscreteObserver

--- src/model/DiscreteObserver.java
+++ src/model/DiscreteObserver.java
@@ -1,5 +1,26 @@
 package model;
 
-public class DiscreteObserver {
+import org.mklab.nfc.matrix.DoubleComplexMatrix;
 
+/**
+ * (1重)倒立振子のりサン時間オブザーバを表すクラスです
+ * 
+ * @author arabian9ts
+ *
+ */
+public class DiscreteObserver extends org.mklab.tool.control.system.controller.DiscreteObserver {
+
+	/**
+	 * 新しく生成された<code>DiscreteObserver</code>オブジェクトを初期化します
+	 */
+	public DiscreteObserver() {
+		super(new LinearPendulum());
+		setContinuousObserverPoles(new DoubleComplexMatrix(
+				new double[]{-2, -2},
+				new double[]{0, 0}).transpose());
+		
+		setSamplingInterval(0.005);
+	}
+	
+	
 }
