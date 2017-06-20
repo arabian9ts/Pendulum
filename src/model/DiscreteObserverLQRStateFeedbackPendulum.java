4dbcb77b591b09edc8fe753a5270bb326df007fa
arabian9ts <nok14.tm59@gmail.com>
2017-06-20 12:47:24 +0900

    implement: src/model/DiscreteObserverLQRStateFeedbackPendulum

--- src/model/DiscreteObserverLQRStateFeedbackPendulum.java
+++ src/model/DiscreteObserverLQRStateFeedbackPendulum.java
@@ -1,5 +1,26 @@
 package model;
 
-public class DiscreteObserverLQRStateFeedbackPendulum {
+import org.mklab.nfc.matrix.DoubleMatrix;
+import org.mklab.tool.control.system.SystemOperator;
 
+/**
+ * 倒立振子の(LQR最適制御に基づく状態フィードバック+離散時間オブザーバ)による安定化制御のシステムを生成するクラスです
+ * 
+ * @author arabian9ts
+ *
+ */
+public class DiscreteObserverLQRStateFeedbackPendulum {
+	
+	/**
+	 * 倒立振子の(LQR最適制御に基づく状態フィードバック+離散時間オブザーバ)による安定化制御のシステムを返します
+	 * @return 倒立振子の(LQR最適制御に基づく状態フィードバック+離散時間オブザーバ)による安定化制御のシステム
+	 */
+	public static SystemOperator getInstance(){
+		LqrStateFeedback stateFeedback = new LqrStateFeedback();
+		DoubleMatrix Q = DoubleMatrix.diagonal(new double[]{1, 1, 1, 1});
+		DoubleMatrix R = new DoubleMatrix(new double[]{1});
+		stateFeedback.setWeightingMatrices(Q, R);
+		
+		return DiscreteObserverStateFeedbackPendulum.getInstance(stateFeedback);
+	}
 }
