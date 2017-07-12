2b044e48dc2c42ea30793156b554e568ae85562b
arabian9ts <nok14.tm59@gmail.com>
2017-07-13 06:29:35 +0900

    modify: converter

--- res/convert.sh
+++ res/convert.sh
@@ -1,6 +1,6 @@
 read -sp "password: " pass;
 
-for name in `ls | grep -v $0`; do
+for name in *.png; do
     name=$(echo $name | sed 's/\.png//g');
     echo $pass | sudo -S convert $name.png $name.eps;
 done
